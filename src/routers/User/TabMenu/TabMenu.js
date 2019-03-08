41ec97af0a85cf18fa60452c471980a7a8fb5a81
ImRTee <imrtee8@gmail.com>
2019-03-08 19:57:01 +1100

    Active Link Style Implemented

--- src/routers/User/TabMenu/TabMenu.js
+++ src/routers/User/TabMenu/TabMenu.js
@@ -1,41 +1,37 @@
 import React, { Component } from "react";
 import styles from "./styles.module.css";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { Icon } from "semantic-ui-react";
 export default class TabMenu extends Component {
   render() {
-    //Check the history path from props to active the menu hightlight
-    console.log(this.props);
-    let isHome = false;
-    const { pathname } = this.props;
-    console.log(this.props.match);
-    pathname && pathname === "/home" ? (isHome = true) : (isHome = false);
     return (
-      <div className={styles.container}>
-        {/* <Link to="/"> */}
-        <Link
-          to="/"
-          className={`${styles.item} ${isHome === true ? styles.active : ""}`}
-        >
-          <Icon name="home" size="large" />
-          <div className={styles.text}>Home</div>
-        </Link>
-        {/* </Link> */}
+      <ul className={styles.container}>
+          <li className={styles.item}>
+            <NavLink to="/home" activeStyle={{color: '#f2b134'}}>
+              <Icon name="home" size="large" />
+              <div className={styles.text}>Home</div>
+            </NavLink>
+          </li>
+          <li className={styles.item}>
+              <NavLink to="/category" activeStyle={{color: '#f2b134'}}>
+                  <Icon name="list ul" size="large" />
+                  <div className={styles.text}>Category</div>
+              </NavLink>
+          </li>
 
-        <Link to="/category" className={styles.item}>
-          <Icon name="list ul" size="large" />
-          <div className={styles.text}>Category</div>
-        </Link>
-
-        <Link to="profile" className={styles.item}>
-          <Icon name="user" size="large" />
-          <div className={styles.text}>Profile</div>
-        </Link>
-        <Link to="/more" className={styles.item}>
-          <Icon name="sign-out" size="large" />
-          <div className={styles.text}>More</div>
-        </Link>
-      </div>
+          <li className={styles.item}>
+            <NavLink to="/profile" activeStyle={{color: '#f2b134'}}>
+              <Icon name="user" size="large" />
+              <div className={styles.text}>Profile</div>
+            </NavLink>
+          </li>
+          <li className={styles.item}>
+            <NavLink to="/more" activeStyle={{color: '#f2b134'}}>
+              <Icon name="sign-out" size="large" />
+              <div className={styles.text}>More</div>
+            </NavLink>
+          </li>
+      </ul>
     );
   }
 }
