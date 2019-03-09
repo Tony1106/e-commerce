f9060f3373ee12f4f901a91cd16e583bdef003c4
Tony1106 <buiminhtien06@gmail.com>
2019-03-09 11:45:16 +1100

    create profile page

--- src/screens/users/Profile/Profile.js
+++ src/screens/users/Profile/Profile.js
@@ -1,7 +1,20 @@
 import React, { Component } from "react";
-
+import styles from "./styles.module.css";
 export default class Profile extends Component {
   render() {
-    return <div>Profile</div>;
+    return (
+      <div className={styles.container}>
+        <div className={styles.avatar}>
+          <div className={styles.image} />
+          <div className={styles.name}>Tony Bui</div>
+        </div>
+        <ul className={styles.listMenu}>
+          <li className={styles.menuItem}>My Order</li>
+          <li className={styles.menuItem}>My WishList</li>
+          <li className={styles.menuItem}>My Address</li>
+          <li className={styles.menuItem}>Account Setting</li>
+        </ul>
+      </div>
+    );
   }
 }
